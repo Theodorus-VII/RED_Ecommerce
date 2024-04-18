e26a7220babbb1eac9595f5b0a98da69e1621c23
Theodorus-VII <tediberhanu777@gmail.com>
2024-04-18 08:45:30 +0300

    testing redirect

--- Ecommerce/Controllers/TestController.cs
+++ Ecommerce/Controllers/TestController.cs
@@ -23,7 +23,7 @@ public class TestController : ControllerBase
     public IActionResult GetTestResult()
     {
         
-        return Redirect("https://google.com");
+        return Redirect("https://red-ecommerce.onrender.com/cart");
         // return Ok("Ok from the test controller");
     }
 
