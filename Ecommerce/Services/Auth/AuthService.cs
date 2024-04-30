e819790ed7ccebf36158edaa97024fe0854dde01
Theodorus-VII <tediberhanu777@gmail.com>
2024-04-30 13:18:13 +0300

    fix: confirmation email url

--- Ecommerce/Services/Auth/AuthService.cs
+++ Ecommerce/Services/Auth/AuthService.cs
@@ -386,9 +386,7 @@ public class AuthService : IAuthService
             encodedConfirmationToken);
         
         var email_callbackURL =
-            @$"{scheme}://{baseUrl}{action}?userId={user.Id}
-                &token={encodedConfirmationToken}
-                &callbackUrl={callbackUrl}";
+            $"{scheme}://{baseUrl}{action}?userId={user.Id}&token={encodedConfirmationToken}&callbackUrl={callbackUrl}";
 
         var confirmationEmail = new EmailDto
         {
