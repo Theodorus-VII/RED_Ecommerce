18d148f99e11e332cdaef15996ebaef4bab462ec
Robel Yemane <ryemane2.1@gmail.com>
2024-05-09 09:15:35 +0300

    Added Maximum Price to Response Message

--- Ecommerce/Controllers/Product/Contracts/FilterAttributesResponse.cs
+++ Ecommerce/Controllers/Product/Contracts/FilterAttributesResponse.cs
@@ -3,4 +3,5 @@ public class FilterAttributesResponse{
     public List<ProductDto> ProductDtos{get;set;}=new List<ProductDto>();
     public int NextIndex{get;set;}
     public int Total{get;set;}
+    public float MaximumPrice{get;set;}
 }
