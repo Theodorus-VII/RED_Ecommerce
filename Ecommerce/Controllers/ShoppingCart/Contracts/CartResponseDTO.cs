ac531b548c76e70f8face82142eb6cb2978a3030
Zekariyas-Teshager <zekariyasteshager@gmail.com>
2024-05-20 09:50:20 +0300

    image added to cart response.

--- Ecommerce/Controllers/ShoppingCart/Contracts/CartResponseDTO.cs
+++ Ecommerce/Controllers/ShoppingCart/Contracts/CartResponseDTO.cs
@@ -14,9 +14,26 @@ namespace Ecommerce.Controllers.ShoppingCart.Contracts
         public int CartItemId { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public Product? Product { get; set; }
+        public ProductResponseDTO? Product { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
 
+    public class ProductResponseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Brand { get; set; } = null!;
+        public int Count { get; set; } = 1;
+        public string Details { get; set; } = "None";
+        public Category Category { get; set; } = Category.Other;
+        public List<ImageResponseDTO> Images { get; set; } = new List<ImageResponseDTO>();
+        public float Price { get; set; }
+        public List<Rating> Ratings { get; set; } = new List<Rating>();
+    }
+
+    public class ImageResponseDTO
+    {
+        public string? Url { get; set; }
+    }   
 }
